1d6e4f68b8eb2073204a333ef2637b80cca3729c
Eduardo Salinas <elduddits@gmail.com>
2019-09-21 13:34:14 -0700

    - Updates Package to swift-tools-version 5.1 - Updates Package to support iOS 13 and up - Adds initial CDNetworking configuration and methods - Removes failing unit test

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 /**
  *  CDNetworking
@@ -10,6 +10,7 @@ import PackageDescription
 
 let package = Package(
     name: "CDNetworking",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
--- Sources/CDNetworking/CDNetworking.swift
+++ Sources/CDNetworking/CDNetworking.swift
@@ -1,3 +1,43 @@
-struct CDNetworking {
-    var text = "Hello, World!"
+import Foundation
+import Combine
+
+public enum APIError: Error {
+    case statusCode(Int)
+    case invalidResponse
 }
+public struct CDNetworking {
+    let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    public func hit<T: Decodable>(url: URL, completion: @escaping (Result<T,Error>) -> Void) -> AnyCancellable {
+        return session.dataTaskPublisher(for: url)
+        .tryMap { try self.validate(data: $0.data, response: $0.response)
+        }
+        .decode(type: T.self, decoder: JSONDecoder())
+        .eraseToAnyPublisher()
+        .sink(receiveCompletion: {
+            switch $0 {
+            case .finished: ()
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }, receiveValue: { result in
+            completion(.success(result))
+        })
+    }
+
+    func validate(data: Data, response: URLResponse) throws -> Data {
+        guard let response = response as? HTTPURLResponse else {
+            throw APIError.invalidResponse
+        }
+        guard 200..<300 ~= response.statusCode else {
+            throw APIError.statusCode(response.statusCode)
+        }
+        return data
+    }
+}
+
+
